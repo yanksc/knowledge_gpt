4239a6a9a121883504cbbe7ff5e0149fe61bc357
yanksc <yanksc@gmail.com>
2023-05-17 13:51:48 -0700

    change the title as proof of concept

--- knowledge_gpt/components/faq.py
+++ knowledge_gpt/components/faq.py
@@ -6,17 +6,17 @@ def faq():
     st.markdown(
         """
 # FAQ
-## How does KnowledgeGPT work?
+## How does C&C GTM Knowledge GPT work?
 When you upload a document, it will be divided into smaller chunks 
 and stored in a special type of database called a vector index 
 that allows for semantic search and retrieval.
 
-When you ask a question, KnowledgeGPT will search through the
+When you ask a question, C&C GTM Knowledge GPT will search through the
 document chunks and find the most relevant ones using the vector index.
 Then, it will use GPT3 to generate a final answer.
 
 ## Is my data safe?
-Yes, your data is safe. KnowledgeGPT does not store your documents or
+Yes, your data is safe. this does not store your documents or
 questions. All uploaded data is deleted after you close the browser tab.
 
 ## Why does it take so long to index my document?
@@ -31,15 +31,15 @@ the chunk number on that page. For DOCS and TXT documents,
 the first number is set to 1 and the second number is the chunk number.
 
 ## Are the answers 100% accurate?
-No, the answers are not 100% accurate. KnowledgeGPT uses GPT-3 to generate
+No, the answers are not 100% accurate. C&C GTM Knowledge GPT uses GPT-3 to generate
 answers. GPT-3 is a powerful language model, but it sometimes makes mistakes 
-and is prone to hallucinations. Also, KnowledgeGPT uses semantic search
+and is prone to hallucinations. Also, C&C GTM Knowledge GPT uses semantic search
 to find the most relevant chunks and does not see the entire document,
 which means that it may not be able to find all the relevant information and
 may not be able to answer all questions (especially summary-type questions
 or questions that require a lot of context from the document).
 
-But for most use cases, KnowledgeGPT is very accurate and can answer
+But for most use cases, C&C GTM Knowledge GPT is very accurate and can answer
 most questions. Always check with the sources to make sure that the answers
 are correct.
 """
--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -12,7 +12,7 @@ def sidebar():
         st.markdown(
             "## How to use\n"
             "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
-            "2. Upload a pdf, docx, or txt file📄\n"
+            "2. Upload a pdf, docx, pptx or txt file📄\n"
             "3. Ask a question about the document💬\n"
         )
         api_key_input = st.text_input(
@@ -29,15 +29,15 @@ def sidebar():
         st.markdown("---")
         st.markdown("# About")
         st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
+            "📖C&C BI GMT knowledge GPT allows you to ask questions about your "
             "documents and get accurate answers with instant citations. "
         )
         st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+            "This tool is a proof of concept that  "
+            "You forked from the knowledgeGPT on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
             "with your feedback and suggestions💡"
         )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("forked by YK and originated by [mmz_001](https://twitter.com/mm_sasmitha)")
         st.markdown("---")
 
         faq()
