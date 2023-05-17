9ce7c3167863be45073a4c7c0878cd2b8e85a36a
yanksc <yanksc@gmail.com>
2023-05-17 13:47:26 -0700

    change the response

--- knowledge_gpt/prompts.py
+++ knowledge_gpt/prompts.py
@@ -2,7 +2,7 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """Create a final answer to the given questions using the provided document excerpts(in no particular order) as references. ALWAYS include a "SOURCES" section in your answer including only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not know. Do not attempt to fabricate an answer and leave the SOURCES section empty.
+template = """Create a final answer to the given questions using the provided document excerpts(in no particular order) as references. ALWAYS include a "SOURCES" section in your answer including only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that the answer is not found in the document. Do not attempt to fabricate an answer and leave the SOURCES section empty.
 
 ---------
 
